5bdaad323e41907e191a0cda865915eaf4942fec
Nicolas Judd <nicolas.judd@gmail.com>
2019-11-20 07:57:54 -0500

    Added Docstring to fibonnaci

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,2 +1,17 @@
-def fibonacci(start):
+def fibonacci(start_pos,size):
+    """
+    Produces a segment of all the fibonacci sequence starting at a start position and ending after set number of characters
+    
+    Parameters
+    ----------
+    start_pos : int
+        Index of the value of the Fibonacci sequnce to start with. E.g., 
+    size : int
+        Length of the returned sequence
+
+    Returns
+    -------
+    array-like :
+        Segment of Fibonacci sequence
+    """
     pass
